4efdaa46c8b172edbb4e5be1e1a4b71a3de8162e
Skyress-s <mathmoe64@gmail.com>
2021-09-15 20:03:35 +0200

    Further work

--- GUI2.py
+++ GUI2.py
@@ -16,5 +16,13 @@ root.title("Kiosken SIMPLIFIED") # adds the title
 canvas = tk.Canvas(root, height=750, width=1100, bg=backgroundColor)
 canvas.pack(fill="both", expand="true")
 
+frame = tk.Frame(root, bg=foreground)
+frame.place(relwidth=0.9, relheight=0.9, relx=0.05, rely=0.05)
+
+label1 = tk.Label(frame, text="hello")
+label1.grid(row=0, column=4, padx = 10, pady = 10)
+
+label2 = tk.Label(frame, text="hello")
+label2.grid(row=0, column=5, padx = 10, pady = 10)
 
 root.mainloop()
