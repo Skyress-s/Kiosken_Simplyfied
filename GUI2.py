7612abb72239f59d8c1abeede25e2f6d0e6edb88
Madrass <Madrass@LAPTOP-GE9TSN73>
2021-09-16 10:56:48 +0200

    further work

--- GUI2.py
+++ GUI2.py
@@ -13,7 +13,7 @@ root = tk.Tk()
 root.title("Kiosken SIMPLIFIED") # adds the title
 
 
-canvas = tk.Canvas(root, height=750, width=1100, bg=backgroundColor)
+canvas = tk.Canvas(root, height=750, width=1300, bg=backgroundColor)
 canvas.pack(fill="both", expand="true")
 
 frame = tk.Frame(root, bg=foreground)
@@ -30,7 +30,9 @@ label2.grid(row=0, column=5, padx = 10, pady = 10)
 
 
 
-
+def placeholder(num):
+    #do something
+    test = num
 #creates the item buttons
 for i in range(0, len(items)):
     padding = 10
@@ -42,12 +44,12 @@ for i in range(0, len(items)):
     #def shellfunc(tmp):  # updates the label AND makes the purchase
        # purchase(tmp)
        # WriteToLabel()
-
-
-    item2 = tk.Button(frame, text=items[i][0], bd=0, width=12, height=2, fg="white", bg=GeneralButtonColor,
-                      command=lambda tmp=i: #shellfunc(tmp))
+    item2 = tk.Button(frame, text=str(i), bd=0, width=12, height=2, fg="white", bg=GeneralButtonColor,
+                      command=lambda tmp=i: placeholder(4))#shellfunc(tmp))
     # by using a temp value tmp, it will stay induvidual for each button :)
     item2.place(anchor="nw", x=x*100 + padding, y=y*50 + padding)
+
+
 
 
 
--- main2.py
+++ main2.py
@@ -1,8 +1,12 @@
 from datetime import datetime
 import os
 
+currentCart = 0
+TotalSession = 0
+actualItems = 0
 
-items = {1,2,3,4,5,6}
+
+items = {1,2,3,4,5,6,7,8,9,0,11,12,13,14,15,16,17}
 
 def GetConfig(index = 0):
     file = open("config.txt", "r")
@@ -11,5 +15,22 @@ def GetConfig(index = 0):
     file = file.replace(" ", "")  # removes spaces
     file = file.replace("\n", "")  # removes enter/\n
     file = file.split("=") # splits
-    file = file[1] # gets the value
+    file = file[1] # gets the value/the second element
     return file
+
+def NextCustomer():
+    nextc = 1
+    #goes to next customer
+
+def AddToCart():
+    addt = 1
+
+def ClearCart():
+    c = 1
+
+# external data/serializing
+def SerializeItems():
+    s = 1
+
+def LoadFromData():
+    l = 1
