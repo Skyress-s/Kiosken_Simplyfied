c20d9085e72787de0d99b2c98432e06b36efb5c7
Madrass <Madrass@LAPTOP-GE9TSN73>
2021-09-16 13:51:00 +0200

    Added most of the buttons, cart, add item, item buttons and switchPaymentMethod is now fully functional

--- GUI2.py
+++ GUI2.py
@@ -25,35 +25,34 @@ frame = tk.Frame(root, bg=foreground)
 frame.place(relwidth=0.9, relheight=0.9, relx=0.05, rely=0.05)
 
 
-    # ADD ITEM FRAME
-AdditemFrame = tk.Frame(frame, bg =groupingColor)
+def PlaceAddNewItenButton():
+    AdditemFrame = tk.Frame(frame, bg=groupingColor)
 
-AdditemFrame.place(relx=0.97, rely=0.95,
-                   height=70, width=150,
-                   anchor="se")
+    AdditemFrame.place(relx=0.97, rely=0.95,
+                       height=70, width=150,
+                       anchor="se")
 
-E1 = tk.Entry(AdditemFrame, bd = 2, width=20)
-E1.pack(side="top")
-
-
-def OnAddNewItemClick(Entry = tk.Entry):
-    input = Entry.get()
-    input = str(input)
-    input = input.split(',')
-    AddNewItemToItemsFile(input[0], input[1])
-
-    Entry.delete(0, 'end')
-
-AddNewItem = tk.Button(AdditemFrame, bd=0, text="Add New Item", padx=5, pady=5, fg="white", bg=GeneralButtonColor, command=lambda: OnAddNewItemClick(E1))
-AddNewItem.pack(side="bottom")
+    E1 = tk.Entry(AdditemFrame, bd=2, width=20)
+    E1.pack(side="top")
 
 
+    def OnAddNewItemClick(Entry = tk.Entry):
+        input = Entry.get()
+        input = str(input)
+        input = input.split(',')
+        AddNewItemToItemsFile(input[0], input[1])
 
+        Entry.delete(0, 'end')
 
+    AddNewItem = tk.Button(AdditemFrame, bd=0, text="Add New Item", padx=5, pady=5, fg="white", bg=GeneralButtonColor, command=lambda: OnAddNewItemClick(E1))
+    AddNewItem.pack(side="bottom")
+
+PlaceAddNewItenButton()
 
 def PlaceItemButtons():
-    def placeholder(num):
-        test = 1
+    def placeholder(num = 0):
+        AddToCart(num)
+        WriteToCurrentItemsView()
 
     items = LoadItemsFromData()
 
@@ -72,7 +71,7 @@ def PlaceItemButtons():
 
 
         item2 = tk.Button(frame, text=allItems[i][0], bd=0, width=12, height=2, fg="white", bg=GeneralButtonColor,
-                          command=lambda tmp=i: placeholder(4))
+                          command=lambda tmp=i: placeholder(tmp))
 
         # by using a temp value tmp, it will stay induvidual for each button :)
         item2.place(anchor="nw", x=x*100 + padding, y=y*50 + padding)
@@ -80,26 +79,89 @@ def PlaceItemButtons():
 PlaceItemButtons()
 
 
-def CurrentItemsView():
-    # current items
-    currentItems = tk.Label(frame, text="helwo,\n nå har du åpnet appen, \ndu trykker på items for å \nlegge dem til i handle" +
-                            " \nkurven og \ntrykker på neste kunde\n for å legge det i loggen \n" +
-                            " og til neste kunde, \n enjoy :)\n" +
-                            "skriv in nye items \n på denne måten\n"+
-                            "gjenstand,pris", width=25, height=30, fg="white", bg=groupingColor)
-    currentItems.place( anchor="s", rely=0.95, relx=0.5, relheight=0.8)
+#def CurrentItemsView():
+# current items
+currentItems = tk.Label(frame, text="helwo,\n nå har du åpnet appen, \ndu trykker på items for å \nlegge dem til i handle" +
+                        " \nkurven og \ntrykker på neste kunde\n for å legge det i loggen \n" +
+                        " og til neste kunde, \n enjoy :)\n" +
+                        "skriv in nye items \n på denne måten\n"+
+                        "gjenstand,pris", width=25, height=30, fg="white", bg=groupingColor)
+currentItems.place( anchor="s", rely=0.95, relx=0.5, relheight=0.8)
+#CurrentItemsView()
 
-CurrentItemsView()
+def WriteToCurrentItemsView():
+    items = LoadItemsFromData()
+    cart = GetCurrentCart()
+    s = ""
+    # prints all the items and the amount of them
+    for i in range(0,len(items)):
+        if cart[i] > 0:
+            s += items[i][0] + " - "
+            s += str(cart[i])
+            s += '\n'
+
+    #adds the sum of all current items thus far
+    s += '\n'
+    price = 0
+    for i in range(0, len(items)):
+        price += cart[i] * items[i][1]
+
+    s += str(price)
+
+
+    currentItems.config(text=s)
 
 
 def PaymentMethodButton():
-    def dettt():
-        t = 1
+
+    def OnClick(button = tk.Button):
+        if  GetCurrentPaymentMethod() == PaymentMethod.Vipps:
+            SetCurrentPaymentMethod(PaymentMethod.Kontant)
+            button.config(text = "Kontant")
+            button.config(bg=GetConfig(10))
+
+        elif GetCurrentPaymentMethod() == PaymentMethod.Kontant:
+            SetCurrentPaymentMethod(PaymentMethod.Kort)
+            button.config(text = "Kort")
+            button.config(bg=GetConfig(11))
+
+        elif GetCurrentPaymentMethod() == PaymentMethod.Kort:
+            SetCurrentPaymentMethod(PaymentMethod.Vipps)
+            button.config(text = "Vipps")
+            button.config(bg=GetConfig(9))
 
 
-    BToggleVipps = tk.Button(frame, text="Vipps", bd=0, width=20, height=3, fg="white", bg="#FF5B24",
-                             command=lambda: dettt())
+
+    BToggleVipps = tk.Button(frame, text="Vipps", bd=0, width=20, height=3, fg="white", bg=GetConfig(9),
+                             command=lambda: OnClick(BToggleVipps))
     BToggleVipps.place(anchor="ne", relx=0.49, rely=0.03)
 PaymentMethodButton()
+
+
+def ClearCartButton():
+
+    def OnClick():
+        InitAndResetCart()
+        WriteToCurrentItemsView()
+
+    clearButtonColor = GetConfig(3)
+    BClear = tk.Button(frame, text="Clear", bd=0, width=12, height=2, fg="white", bg=clearButtonColor,
+                       command=lambda: OnClick())
+    BClear.place(anchor="e", relx=0.97, rely=0.5)
+ClearCartButton()
+
+
+def PlaceNextCustomerutton():
+
+    def OnClick():
+        t =1
+    BNextCostomer = tk.Button(frame, text="Next costumer", bd=0, width=20, height=3, fg="white", bg=GetConfig(2),
+                              command=lambda: OnClick())
+    #BNextCostomer.pack()
+    BNextCostomer.place(anchor="nw", relx=0.51, rely=0.03)
+PlaceNextCustomerutton()
+
+
+
 
 root.mainloop()
--- main2.py
+++ main2.py
@@ -1,12 +1,50 @@
 from datetime import datetime
 import os
 from enum import Enum
-
+# all global items are defined here
 ItemsPath = 'Items.txt'
 
-currentCart = 0
+currentCart = [] # has this form currentcart[index][currentPaymentMethod] ie. index 0 = solo 1 = pepsi, cPM it with slott
+# each index has three slots, one for each payment type
+
 TotalSession = 0
 actualItems = 0
+
+def LoadItemsFromData():
+    file = open(ItemsPath, 'r')
+    items = file.readlines() # reads all the lines and passes them to an array
+    items = [item.replace('\n', '') for item in items]
+
+    for x in range(0, len(items)):
+        app = items[x] # gets the element from the item list
+        app = app.split(',') # spilts it into an array with     ['name', 'price']
+        app[1] = int(app[1]) # converts price to int            ['name', price]
+        items[x] = app
+
+    return items
+
+def InitAndResetCart():
+    global currentCart
+    currentCart.clear()
+    for i in range(0, len(LoadItemsFromData())):
+        currentCart.append(0)
+    print(currentCart)
+
+InitAndResetCart()
+
+
+def AddToCart(num):
+    global currentCart
+    global currentPaymentMethod
+    currentCart[num] += 1
+    print(currentCart)
+
+def ClearCart():
+    c = 1
+
+def GetCurrentCart():
+    global currentCart
+    return currentCart
 
 
 def GetConfig(index = 0):
@@ -23,28 +61,13 @@ def NextCustomer():
     nextc = 1
     #goes to next customer
 
-def AddToCart():
-    addt = 1
 
-def ClearCart():
-    c = 1
 
 # external data/serializing
 def SerializeItems():
     s = 1
 
-def LoadItemsFromData():
-    file = open(ItemsPath, 'r')
-    items = file.readlines() # reads all the lines and passes them to an array
-    items = [item.replace('\n', '') for item in items]
 
-    for x in range(0, len(items)):
-        app = items[x] # gets the element from the item list
-        app = app.split(',') # spilts it into an array with     ['name', 'price']
-        app[1] = int(app[1]) # converts price to int            ['name', price]
-        items[x] = app
-
-    return items
 
 def AddNewItemToItemsFile(name = "", price = 10):
     file = open(ItemsPath, 'a')
@@ -59,9 +82,25 @@ def CheckIfFileExits(string = ""):
 
 
 
-class CurrentPaymentMethod(Enum):
-    Vipps = 1
-    Kontant = 2
-    Kort = 3
+class PaymentMethod(Enum):
+    Vipps = 0
+    Kontant = 1
+    Kort = 2
 
-print(CurrentPaymentMethod.Kort.value)
+#defualt value is Vipps
+currentPaymentMethod = PaymentMethod.Vipps
+
+def GetCurrentPaymentMethod():
+    global currentPaymentMethod
+    return currentPaymentMethod
+
+def SetCurrentPaymentMethod(method = PaymentMethod):
+    global currentPaymentMethod
+    currentPaymentMethod = method
+
+
+#print(GetCurrentPaymentMethod().value)
+#SetCurrentPaymentMethod(PaymentMethod.Kontant)
+#print(GetCurrentPaymentMethod().value)
+
+
